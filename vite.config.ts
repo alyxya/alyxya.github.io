ec1cdbf4b514542f8de71356fa4814ff469b024d
alyxya <hi@alyxya.com>
2026-03-18 15:19:45 -0700

    Raise Vite chunk warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		chunkSizeWarningLimit: 700
+	},
 	server: {
 		host: '127.0.0.1',
 		watch: {
